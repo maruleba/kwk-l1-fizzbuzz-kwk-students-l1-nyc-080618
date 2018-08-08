40c6749d5c41f4c33b39c66b0efd39262c2c28b4
maruleba <folaa1234@gmail.com>
2018-08-08 14:19:15 +0000

    automatically backed up by learn

--- dancing.rb
+++ dancing.rb
@@ -1,12 +1,7 @@
 #write a while loop 
 # output "step to the left"/ step to the right" 5 times 
 
-counter = 0 
-while counter > 5 
-puts"step to the right"
-puts "step to the left" 
-counter =  counter + 1
-end
+
 
 play_count = 0
 while play_count < 5
